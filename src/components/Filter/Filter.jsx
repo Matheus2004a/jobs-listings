1dd7bc140bd20d3b0343eb987aa113726c424385
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-07-22 20:40:03 -0300

    :sparkles: feat: Remove barra de filtros Closes #1

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -6,24 +6,25 @@ const Filter = (props) => {
     const [filterTags, setFilterTags] = useState([])
 
     function removeFilterById(index) {
-        let tagsRemoved = props.typesFilters.splice(index, 1)
+        const tagsRemoved = props.typesFilters.splice(index, 1)
         setFilterTags([...filterTags, tagsRemoved])
+        if (props.typesFilters.length === 0) {
+            props.deleteFilter()
+        }
     }
-
+    
     return (
         <section className="filter-box">
             <div className="container-tool">
                 {filterTags && props.typesFilters.map((item, index) => (
-                    <>
-                        <div className={`filter-tool`} key={index}>
-                            <span>{item}</span>
-                            <button onClick={() => removeFilterById(index)}>
-                                <figure>
-                                    <img src={iconClose} alt="icon-close" />
-                                </figure>
-                            </button>
-                        </div>
-                    </>
+                    <div className="filter-tool" key={index}>
+                        <span>{item}</span>
+                        <button onClick={() => removeFilterById(index)}>
+                            <figure>
+                                <img src={iconClose} alt="icon-close" />
+                            </figure>
+                        </button>
+                    </div>
                 ))}
             </div>
             <button className="button-filter" onClick={props.deleteFilter}>Clear</button>
