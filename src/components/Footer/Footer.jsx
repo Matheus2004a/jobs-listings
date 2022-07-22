042fbcad3424d7d4c1134d4208ce43c1d505ede9
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-07-22 20:44:20 -0300

    :sparkles: feat: Adiciona link para repo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer class="attribution">
+        <footer className="attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-            Coded by <a href="#" target='_blank'>Matheus Aurélio</a>.
+            Coded by <a href="https://github.com/Matheus2004a/jobs-listings" target='_blank'>Matheus Aurélio</a>.
         </footer>
     )
 }
