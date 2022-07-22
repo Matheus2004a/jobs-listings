30e18b22aaacabe16fc680f6f29733f477dd6317
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-07-22 13:37:53 -0300

    :sparkles: feat: Remove filtros específicos

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,21 +1,29 @@
+import { useState } from "react"
 import iconClose from "../../../images/icon-remove.svg"
 import "./Filter.css"
 
 const Filter = (props) => {
+    const [filterTags, setFilterTags] = useState([])
+
+    function removeFilterById(index) {
+        let tagsRemoved = props.typesFilters.splice(index, 1)
+        setFilterTags([...filterTags, tagsRemoved])
+    }
+
     return (
         <section className="filter-box">
             <div className="container-tool">
-                {props.name.map((item, index) => (
-                    <div className="filter-tool">
-                        <>
-                            <span key={index}>{item}</span>
-                            <button>
+                {filterTags && props.typesFilters.map((item, index) => (
+                    <>
+                        <div className={`filter-tool`} key={index}>
+                            <span>{item}</span>
+                            <button onClick={() => removeFilterById(index)}>
                                 <figure>
-                                    <img src={iconClose} alt="" />
+                                    <img src={iconClose} alt="icon-close" />
                                 </figure>
                             </button>
-                        </>
-                    </div>
+                        </div>
+                    </>
                 ))}
             </div>
             <button className="button-filter" onClick={props.deleteFilter}>Clear</button>
